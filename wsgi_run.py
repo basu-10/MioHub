9aadfdf7ff57d57576cc67a659b513baf828b751
basu-10 <asesh.basu.10@gmail.com>
2026-01-13 20:52:22 +0530

    fixing env vars, secrets 2

--- wsgi_run.py
+++
@@ -1,7 +0,0 @@
-from flask_app import app
-from waitress import serve
-
-if __name__ == "__main__":
-    print("Starting WSGI server with Waitress...")
-    print("serving on http://localhost:5555")
-    serve(app, host='0.0.0.0', port=5555)
